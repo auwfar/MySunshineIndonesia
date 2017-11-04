7a40d7a5c9dfa1d80012f3c21b2c134eeed6e607
auwfar <achmad.chadilauwfar@gmail.com>
2017-11-04 16:11:37 +0700

    Tambah Library dan buat RecyclerView

--- app/src/main/java/com/auwfar/mysunshineindonesia/MainActivity.java
+++ app/src/main/java/com/auwfar/mysunshineindonesia/MainActivity.java
@@ -2,27 +2,38 @@ package com.auwfar.mysunshineindonesia;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
-    TextView weatherDayTitle, weatherDesc, weatherTemperature;
-    ImageView imgWeather;
+    @BindView(R.id.weather_day_title) TextView weatherDayTitle;
+    @BindView(R.id.img_weather) ImageView imgWeather;
+    @BindView(R.id.weather_desc) TextView weatherDesc;
+    @BindView(R.id.weather_termperature) TextView weatherTemperature;
+    @BindView(R.id.weather_list) RecyclerView weatherList;
+    private WeatherAdapter weatherAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        weatherDayTitle     = (TextView) findViewById(R.id.weather_day_title);
-        weatherDesc         = (TextView) findViewById(R.id.weather_desc);
-        weatherTemperature  = (TextView) findViewById(R.id.weather_termperature);
-        imgWeather          = (ImageView) findViewById(R.id.img_weather);
+        ButterKnife.bind(this);
 
         weatherDayTitle.setText("Hari Minggu");
         weatherDesc.setText("Hari Terang Benderang");
         weatherTemperature.setText("30 derajat aja");
 
         imgWeather.setImageResource(R.mipmap.ic_launcher_round);
+
+        weatherList.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+
+        weatherAdapter = new WeatherAdapter();
+        weatherList.setAdapter(weatherAdapter);
     }
 }
